3b2f141463a3caa99b538677c5e8e9014af00675
Lauri Ojansivu <x@xet7.org>
2025-12-10 04:19:34 +0200

    Translated to English

--- kanban.c
+++ kanban.c
@@ -2,28 +2,28 @@
 #include <conio.h>   // cputsxy, clrscr, textcolor, cgetc
 #include <c64.h>     // COLOR_RED, COLOR_BLUE jne.
 
-// --- GLOBAALIT MÄÄRITTELYT ---
+// --- GLOBAL DEFINITIONS ---
 
-// Määritellään Color RAM osoitin suoraan char-tyypiksi.
+// Define the Color RAM pointer directly as a char pointer.
 const char* COLOR_RAM_START = (const char*)0xD800;
 
-// VIC-II rekisteri taustavärille (0xD021)
+// VIC-II register for background color (0xD021)
 #define VIC_BG_COLOR (*(char*)0xD021)
 
 #define SCREEN_WIDTH 40
 
-// --- TÄRKEÄ KORJAUS: Määritellään rivimuuttuja globaaliksi ---
+// --- IMPORTANT FIX: Define the row variable as global ---
 int current_row = 1; 
 // -----------------------------------------------------------------
 
-// --- FUNKTIOIDEN MÄÄRITTELYT ---
+// --- FUNCTION DEFINITIONS ---
 
-// Asettaa tietyn sarakkeen osan värin suoraan Värimuistiin
+// Sets the color for a part of a column directly into Color RAM
 void set_color_direct(int x, int y, int length, unsigned char c64_color) {
     int offset = y * SCREEN_WIDTH + x;  
     int i;
     
-    // Kirjoitetaan väriarvo suoraan Color RAMiin käyttämällä osoitinlaskentaa.
+    // Write the color value directly into the Color RAM using pointer arithmetic.
     for (i = 0; i < length; i++) {
         *( (char*)COLOR_RAM_START + offset + i ) = c64_color;
     }
@@ -34,64 +34,64 @@ void print_separator_row(int row) {
 }
 
 void print_header_row(int row) {
-    // 1. Tulosta tekstit (oletusväri)
-    cputsxy(2, row, "TEHTAVAT");
-    cputsxy(14, row, "KESKEN");
-    cputsxy(26, row, "VALMIS");
+    // 1. Print the texts (default color)
+    cputsxy(2, row, "TASKS");
+    cputsxy(14, row, "IN PROGRESS");
+    cputsxy(26, row, "DONE");
     
-    // 2. Aseta värit POKE:lla tekstin päälle
+    // 2. Set colors in the Color RAM over the text
     set_color_direct(2, row, 8, COLOR_CYAN); 
-    set_color_direct(14, row, 6, COLOR_CYAN); 
-    set_color_direct(26, row, 6, COLOR_CYAN); 
+    set_color_direct(14, row, 10, COLOR_CYAN); 
+    set_color_direct(26, row, 4, COLOR_CYAN); 
 }
 
 void print_task_row(int row, const char *todo, const char *doing, const char *done) {
     
-    // Erotinviivat (oletusväri)
+    // Separators (default color)
     cputsxy(1, row, "|"); 
     cputsxy(12, row, "|"); 
     cputsxy(24, row, "|"); 
     cputsxy(36, row, "|"); 
     
-    // 1. Tehtävät (TODO) - Tulostus
+    // 1. Tasks (TODO) - Print
     cputsxy(2, row, todo);
     set_color_direct(2, row, 10, COLOR_RED); 
 
-    // 2. Kesken (DOING) - Tulostus
+    // 2. In Progress (DOING) - Print
     cputsxy(14, row, doing);
     set_color_direct(14, row, 10, COLOR_YELLOW);
     
-    // 3. Valmis (DONE) - Tulostus
+    // 3. Done (DONE) - Print
     cputsxy(26, row, done);
     set_color_direct(26, row, 10, COLOR_GREEN); 
 }
 
 int main(void) {
     
-    // Asetetaan näytön taustaväri Siniseksi.
+    // Set the screen background color to blue.
     VIC_BG_COLOR = COLOR_BLUE; 
     
     clrscr(); 
     textcolor(COLOR_WHITE);
 
-    // Huom! current_row on nyt määritelty globaalisti, joten se ALKAA riviltä 1.
-    // Emme määrittele sitä enää tässä.
+    // Note: current_row is now defined globally, so it STARTS at row 1.
+    // We don't define it here anymore.
 
-    // Otsikko-osa
+    // Header area
     print_separator_row(current_row++);
     print_header_row(current_row++);
     print_separator_row(current_row++);
     
-    // Tehtävien rivit
-    print_task_row(current_row++, "A. UI Desi", "B. Koodi", "C. Testi");
-    print_task_row(current_row++, "D. Refa", "E. Kesken", "");
-    print_task_row(current_row++, "F. Idea", "", "G. Tehty");
+    // Task rows
+    print_task_row(current_row++, "A. UI Desi", "B. Code", "C. Test");
+    print_task_row(current_row++, "D. Refac", "E. In Prog", "");
+    print_task_row(current_row++, "F. Idea", "", "G. Done");
     
-    // Viimeinen erotin
+    // Final separator
     print_separator_row(current_row++);
     
-    // Lopetusviesti
-    cputsxy(0, 24, "Paina mita tahansa nappainta...");
+    // Exit message
+    cputsxy(0, 24, "Press any key...");
     
     cgetc(); 
     
